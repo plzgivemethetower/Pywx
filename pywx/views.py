13ea32dd7070cda18e69a90039d25386e728b032
IceTouch <plzgivemethetower@hotmail.com>
2017-05-03 23:37:32 +0800

    主页测试

--- pywx/views.py
+++ pywx/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# coding:utf-8
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(u"Hello World!")
