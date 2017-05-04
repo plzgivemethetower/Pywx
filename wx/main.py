a5e7a32dfac008e64d3755a89cc8596285e72de5
IceTouch <plzgivemethetower@hotmail.com>
2017-05-04 18:27:29 +0800

    自动回复

--- wx/main.py
+++ wx/main.py
@@ -6,11 +6,11 @@ robot.config["APP_SECRET"] = "628c6b7f77c9f0ad356174ddd6ad2c64"
 robot.config['ENCODING_AES_KEY'] = 'zDSkLBNeNTNd5uku0JfeOJkT5H34GirtH8sMeJ97BvY'
 @robot.text
 def hello(message):
-    if message == "小黄歌":
+    if message.content == "小黄歌":
         return u'链接: https://pan.baidu.com/s/1eR6rUiI 密码: n4b8'
-    elif message == "追剧":
+    elif message.content == "追剧":
         return u'http://www.myei.cc/show/25824.html?from=timeline\n 将链接拷贝至手机浏览器直接播放即可\nmagnet:?xt=urn:btih:3a6d76a16e86e411ef2df070b8cf817b74ea840f&dn=人民的名义（送审样片）.全集.\n联盟MP4-www.lmp4.cn\n也可将本链接在百度网盘中离线下载、观看\n小编缓存了47集以后的部分，链接如下\n链接: https://pan.baidu.com/s/1bp1ozHP 密码: vv2k'
-    elif message =="B站下载":
+    elif message.content =="B站下载":
         return u'链接: https://pan.baidu.com/s/1dFL3cdz 密码: fc8m'
     else:
         return u'我们的聊天机器人正在部署中，有什么问题和建议可以告诉我们'
